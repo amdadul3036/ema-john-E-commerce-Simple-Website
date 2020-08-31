786a008f84d3ed59ddc897093a4cd334cd5d8242
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-31 00:19:04 -0700

    Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header/Header';
 import Shop from './component/Shop/shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './component/Review/Review';
+import Inventory from './component/Inventory/Inventory';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
-      
+        <Router>
+          <Switch>
+             <Route path="/shop">
+                 <Shop></Shop>
+             </Route>
+
+             <Route path="/review">
+                 <Review></Review>
+             </Route>
+
+             <Route path="/inventory">
+                 <Inventory></Inventory>
+             </Route>
+          </Switch>
+        </Router>
+
     </div>
   );
 }
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <nav>
                 <a href="/shop">Shop</a>
                 <a href="/review">Review</a>
-                <a href="/manage">Manage Iventory</a>
+                <a href="/inventory">Manage Iventory</a>
             </nav>
         </div>
     );
