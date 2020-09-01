deb3a420918551eb6cc3e0c773287b0baa103b03
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-31 23:54:53 -0700

    UseParams()

--- src/component/ProductDetail/ProductDetail.js
+++ src/component/ProductDetail/ProductDetail.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import fakeData from '../../fakeData';
 
 const ProductDetail = () => {
+    const {productKey} = useParams();
+    const product = fakeData.find(pd => pd.key === productKey);
+    console.log(product);
     return (
         <div>
-            <h1>Product Detail Comming Soon</h1>
+            <h1>{productKey} Detail Comming Soon</h1>
         </div>
     );
 };
