2c0f51511604007a71cb61e4a04a5c819d8aa5a1
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-01 04:13:51 -0700

    Adding into review page how many product has been ordered

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
 
 const Review = () => {
+
+
+    const [cart,setCart] = useState([])
+    useEffect(()=> {
+// Cart 
+     const savedCart = getDatabaseCart();
+     const productKeys = Object.keys(savedCart);
+     const cartProducts = productKeys.map( key  => {
+         const product = fakeData.find( pd => pd.key === key);
+         product.quantity = savedCart[key];
+         return product;
+     },[])
+     setCart(cartProducts);
+    })
     return (
         <div>
-            <h1>This is Review</h1>
+            <h1>Cart Items: {cart.length}</h1>
+
         </div>
     );
 };
